b56c8257572a7ed9594306ded3092d89366be1bf
Lauren Laskerr <llask@me.com>
2020-01-06 00:17:37 -0500

    added about us onboarding

--- client/src/components/about/AboutUs.jsx
+++ client/src/components/about/AboutUs.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
-import Hero from './Hero'
-import Section from './Section'
 
 export default class AboutUs extends Component {
   render() {
     return (
       <main className="padding">
-        <Hero />
-        <Section />
+        <div className="hero">
+          <h1>
+            So, you've got a project to manage.<br></br>
+            Well then, we've got a solution.
+          </h1>
+          <h3>
+            Here's the process:
+          </h3>
+          <img src="./onboarding_bigger.png" alt=""/>
+      </div>
       </main>
     )
   }
