e136a0f3c62ebeed96af744ea828f0a6ee1f7b79
Lauren Laskerr <llask@me.com>
2020-03-08 19:52:03 -0400

    signup moved to functional comps/hooks

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -1,85 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthService from './AuthService';
 import Button from '../Button'
 import FlashMessage from '../FlashMessage'
 
-class Signup extends Component {
-  constructor(props){
-    super(props);
-    this.state = { name: '',
-    email: '',
-    password: '',
-    flash: false,
-    message: ''
-    };
-    this.service = new AuthService();
+const Signup = (props) => {
+  const service = new AuthService();
+  const [user, setUser] = useState({name: null, email: null, password: null});
+  const [flash, setFlash] = useState(false);
+  const [message, setMessage] = useState(undefined);
+
+  const handleChange = (event) => {  
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value
+    });
   }
-  
-  handleFormSubmit = (event) => {
+
+  const cancelFlash = () => {
+    setFlash(false);
+  }
+
+  const handleFlash = (message) => {
+    setFlash(true);
+    setMessage(message)
+    setTimeout(() => cancelFlash(), 4000)
+  }
+
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-    const name = this.state.name;
-    const email = this.state.email;
-    const password = this.state.password
-  
-    if (email.includes('@') && email.includes('.')) {
-      this.service.signup(name, email, password)
-      .then( response => {
-          this.setState({
-              name: "",
-              email: "",
-              password: "",
-          });
-          this.props.getUser(response)
-          this.props.history.push('/dashboard')
+
+    if (user.email.includes('@' && '.')) {
+      service.signup(user.name, user.email, user.password)
+      .then(response => {
+          setUser({name: null, email: null, password: null});
+          props.getUser(response)
+          props.history.push('/dashboard')
       })
-      .catch( error => console.log(error) )
+      .catch(error => console.log(error) )
     }
     else {
-      this.handleFlash('email must be a valid email');
+      handleFlash('email must be a valid email');
     }
   }
-  
-  handleChange = (event) => {  
-    const {name, value} = event.target;
-    this.setState({[name]: value});
-  }
 
-  handleFlash = (message) => {
-    this.setState({
-      flash: !this.state.flash,
-      message: message
-    })
-    setTimeout(() => this.cancelFlash(), 4000)
-  }
-
-  cancelFlash = () => {
-    this.setState({
-      flash: !this.state.flash
-    })
-  }
-
-  render(){
-    return(
-      // more code will be added here
-      <div className="signup">
-        <div>
-          <h1>Let's get started.</h1>
-          <p>These projects aren't going to start and organize themselves.</p>
-        </div>
-        <form className="signupform">
-          <h1>Signup</h1>
-          <label>Name*</label>
-          <input type="text" name="name" value={this.state.name} onChange={ e => this.handleChange(e)} required/>
-          <label>Email*</label>
-          <input type="email" name="email" value={this.state.email} onChange={ e => this.handleChange(e)} required/>
-          <label>Password*</label>
-          <input type="password" name="password" value={this.state.password} onChange={ e => this.handleChange(e)} required />
-          <FlashMessage thestyle='flashemail' show={this.state.flash} message={this.state.message} />
-          <Button title="join" className="signupbtn" onClick={this.handleFormSubmit}/>
-        </form>
+  return(
+    <div className="signup">
+      <div>
+        <h1>Let's get started.</h1>
+        <p>These projects aren't going to start and organize themselves.</p>
+      </div>
+      <form className="signupform">
+        <h1>Signup</h1>
+        <label>Name*</label>
+        <input type="text" name="name" value={user.name} onChange={ e => handleChange(e)} required/>
+        <label>Email*</label>
+        <input type="email" name="email" value={user.email} onChange={ e => handleChange(e)} required/>
+        <label>Password*</label>
+        <input type="password" name="password" value={user.password} onChange={ e => handleChange(e)} required />
+        <FlashMessage thestyle='flashemail' show={flash} message={message} />
+        <Button title="join" className="signupbtn" onClick={handleFormSubmit}/>
+      </form>
     </div>
-    )
-  }
+  )
+
 }
 
 export default Signup;
